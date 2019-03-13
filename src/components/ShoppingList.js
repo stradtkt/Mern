ec3edf8c53cda137464738b1fbed56c460ba79ec
Kevin <stradtkt22@gmail.com>
2019-03-13 07:31:18 -0400

    Added some reactstrap and some styles

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -25,12 +25,25 @@ class ShoppingList extends Component {
                                         items: [...state, {id: uuid(), name}]
                                     }))
                                 }
-                            }}></Button>
+                            }}>
+                        Add Item
+                    </Button>
                     <ListGroup>
                         <TransitionGroup className="shopping-list">
                             {items.map(({id, name}) => (
                                 <CSSTransition key={id} timeout={500} classNames="fade">
                                     <ListGroupItem>
+                                        <Button
+                                            className="remove-btn"
+                                            color="danger"
+                                            size="sm"
+                                            onClick={() => {
+                                                this.setState(state => ({
+                                                    items: state.items.filter(item => item.id !== id)
+                                                }));
+                                            }}>
+                                            &times;
+                                        </Button>
                                         {name}
                                     </ListGroupItem>
                                 </CSSTransition>
